2bbfae85f73462a27bbc9fe6f76ddf7688b339c7
Chino Padua <paduachino@gmail.com>
2016-04-26 14:32:41 +0800

    1

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = $_POST['message'];
 $to = 'cibs@coachibs.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Coach Approach - Client's Message:  $name";
 $email_body = "You have received a new message from Coach Approach.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nSubject: $subject\n\nMessage:\n$message";
-$headers = "From: cibs@coachibs.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@coachibs.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
